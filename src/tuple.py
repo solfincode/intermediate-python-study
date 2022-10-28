2fe11cbc310b2d9bd103827d10041e3a2a1b84f8
duo <hypersonolabs@gmail.com>
2022-10-28 16:58:57 -0400

    add tuple code

---
+++ src/tuple.py
@@ -0,0 +1,13 @@
+tupletext = ("apple", "iphone", "mac", "pi")
+print(tupletext)
+
+# index
+print(tupletext.index("apple"))
+
+# selection
+print(tupletext[:2])
+
+# unpacking
+my_type = "type1", "type2", "type3"
+one, two, three = my_type
+print(one)
